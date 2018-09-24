59c34e6e242fc035903dc8249e47eb99c5803919
Timur Shoyzhitov <aren.myfa@yandex.ru>
2018-09-25 00:52:45 +0700

    Added toolbar and search button.

--- app/src/main/java/com/qbz/faa/remindme/MainActivity.java
+++ app/src/main/java/com/qbz/faa/remindme/MainActivity.java
@@ -2,12 +2,32 @@ package com.qbz.faa.remindme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
