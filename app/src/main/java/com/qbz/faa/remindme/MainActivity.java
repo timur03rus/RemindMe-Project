0f22bc3f246f5d9abecb5466155d128f765ecdad
Timur Shoyzhitov <aren.myfa@yandex.ru>
2018-09-25 23:09:08 +0700

    Implemented navigation view and tab navigation with test fragment.

--- app/src/main/java/com/qbz/faa/remindme/MainActivity.java
+++ app/src/main/java/com/qbz/faa/remindme/MainActivity.java
@@ -1,20 +1,35 @@
 package com.qbz.faa.remindme;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
+import com.qbz.faa.remindme.adapter.TabPagerFragmentAdapter;
+
 public class MainActivity extends AppCompatActivity {
-    
+
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
         initToolbar();
+        initNavigationView();
+        initTabs();
+
     }
 
     private void initToolbar() {
@@ -29,5 +44,41 @@ public class MainActivity extends AppCompatActivity {
         });
 
         toolbar.inflateMenu(R.menu.menu);
+    }
+
+    private void initNavigationView() {
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                drawerLayout.closeDrawers();
+                switch (item.getItemId()) {
+                    case R.id.actionNotificationItem:
+                        showNotificationTab();
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    private void initTabs() {
+        viewPager = findViewById(R.id.viewPager);
+        TabPagerFragmentAdapter adapter = new TabPagerFragmentAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void showNotificationTab() {
+        viewPager.setCurrentItem(Constants.TAB_TWO);
     }
 }
---
+++ app/src/main/java/com/qbz/faa/remindme/fragment/ExampleFragment.java
@@ -0,0 +1,32 @@
+package com.qbz.faa.remindme.fragment;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.qbz.faa.remindme.R;
+
+public class ExampleFragment extends Fragment {
+
+    private View view;
+
+    public static ExampleFragment getInstance() {
+        Bundle args = new Bundle();
+        ExampleFragment fragment = new ExampleFragment();
+        fragment.setArguments(args);
+
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_example, container, false);
+        return view;
+    }
+}
